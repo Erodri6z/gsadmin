5a2110cacccedd3177af92a4710c4439e54bf39d
Erodri6z <erikrodriguez2417@gmail.com>
2025-01-17 18:58:33 -0600

    add some bootstrap for basic styling

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,23 @@
-function Login () {
+import { useState } from "react"
+
+function Login (props) {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: ""
+  })
+
+
   return (
     <div>
-      <h1>this is the login</h1>
+      <div class="mb-3 red">
+      <label for="exampleFormControlInput1" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+    </div>
+    <div class="mb-3">
+      <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+      <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+      <button type="button" class="btn btn-primary">Base class</button>
+    </div>
     </div>
   )
 }
