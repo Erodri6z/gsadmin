8c4b332102771b4a6bb39921ec637cc9f4004f96
Erodri6z <erikrodriguez2417@gmail.com>
2025-01-20 15:49:18 -0600

    stub up service functions for login

--- src/services/authService.js
+++ src/services/authService.js
@@ -3,3 +3,26 @@ import * as tokenService from  "./tokenService"
 const BASE_URL = `${process.env.SERVER_URL}/profiles/`
 
 
+async function login(credentials) {
+  try {
+    const res = await fetch (`${BASE_URL}/login`, {
+      method: "POST",
+      headers: {
+        'Content-Type' : "application/json"
+      },
+      body: JSON.stringify(credentials)
+    })
+    const json = await res.json()
+    if (json.token) {
+      tokenService.setToken(json.token)
+    }
+    if (json.error) {
+      throw new Error(json.err)
+    }
+  } catch (err) {
+    throw err  
+  }
+}
+
+
+export { login }
