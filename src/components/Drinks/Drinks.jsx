6aec6a87da5d04bbf1641f4a04996ed302974476
Erodri6z <erikrodriguez2417@gmail.com>
2025-01-24 19:51:46 -0600

    fix the drinks comp

---
+++ src/components/Drinks/Drinks.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react"
+
+
+const Drinks = () => {
+  return(
+    <>
+    <div>
+      <h2>The Drinks will be here</h2>
+      <div>
+        card
+      </div>
+    </div>
+    </>
+  )
+}
+
+
+export default Drinks
