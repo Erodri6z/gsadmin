0eba5fe8365e73aebf6d6642fdc883f957f3bf2d
Erodri6z <erikrodriguez2417@gmail.com>
2025-01-24 19:58:14 -0600

    add a get drinks service

--- src/services/drinksService.js
+++ src/services/drinksService.js
@@ -1,3 +1,27 @@
+import { METHODS } from "http"
+import * as tokenService from  "./tokenService"
 
 
 const BASE_URL = "http://localhost:5047"
+
+async function GetDrinks()  {
+  try {
+    const res = await (`${BASE_URL}/drinks`)
+
+    console.log(await res.json())
+    return await res.json()
+
+
+  } catch (err) {
+    throw err  
+  }
+}
+
+
+
+
+
+
+
+
+export { GetDrinks }
