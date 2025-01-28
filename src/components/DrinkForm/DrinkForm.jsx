2edd764535ce677763c7ed76c9e0bfe77be84bf1
Erodri6z <erikrodriguez2417@gmail.com>
2025-01-27 23:30:59 -0600

    add some styling and fixes for drinksform

--- src/components/DrinkForm/DrinkForm.jsx
+++ src/components/DrinkForm/DrinkForm.jsx
@@ -1,45 +1,71 @@
+import "./DrinkForm.css"
 const DrinkForm = () => {
   return (
     <>
     <div>
       <h2>Drink form</h2>
       <form class="drink-form">
-        <label>Name</label>
-        <input type="text" name="name" /> 
-        <label>Main Spirit</label>
-        <select name="mainSpirit">
-          <options value="">Pick One</options>
-          <options value="Whiskey">Whiskey</options>
-          <options value="Vodka">Vodka</options>
-          <options value="Tequila">Tequila</options>
-          <options value="Gin">Gin</options>
-          <options value="Rum">Rum</options>
-          <options value="Other">Other</options>
-        </select>
-        <label>Image</label>
-        <input type="file" />
-        <label>Ingredients</label>
-        <input type="text" />
-        <label>Measurements</label>
-        <input type="text" />
-        <label>Bitters</label>
-        <input type="text" />
-        <label>Garnisn Options</label>
-        <input type="text" />
-        <label>Color</label>
-        <input type="text" />
-        <label>Recommended Glass</label>
-        <input type="text" />
-        <label>Notes</label>
-        <input type="text" />
-        <label>Method</label>
-        <input type="text" />
-        <label>Credit</label>
-        <input type="text" />
-        <label>Vibe</label>
-        <input type="text" />
-
-
+        <div class="form-entry">
+          <label>Name</label>
+          <input type="text" name="name" /> 
+        </div>
+        <div class="form-entry">
+          <label>Main Spirit</label>
+          <select name="mainSpirit">
+            <option value="">Pick One</option>
+            <option value="Whiskey">Whiskey</option>
+            <option value="Vodka">Vodka</option>
+            <option value="Tequila">Tequila</option>
+            <option value="Gin">Gin</option>
+            <option value="Rum">Rum</option>
+            <option value="Other">Other</option>
+          </select>
+        </div>
+        <div class="form-entry">
+          <label>Image</label>
+          <input type="file" />
+        </div>
+        <div class="form-entry">
+          <label>Ingredients</label>
+          <input type="text" />
+        </div>
+        <div class="form-entry">
+          <label>Measurements</label>
+          <input type="text" />
+        </div>
+        <div class="form-entry">
+          <label>Bitters</label>
+          <input type="text" />
+        </div>
+        <div class="form-entry">
+          <label>Garnisn Options</label>
+          <input type="text" />
+        </div>
+        <div class="form-entry">
+          <label>Color</label>
+          <input type="text" />
+        </div>
+        <div class="form-entry">
+          <label>Recommended Glass</label>
+          <input type="text" />
+        </div>
+        <div class="form-entry">
+          <label>Notes</label>
+          <input type="text" />
+        </div>
+        <div class="form-entry">
+          <label>Method</label>
+          <input type="text" />
+        </div>
+        <div class="form-entry">
+          <label>Credit</label>
+          <input type="text" />
+        </div>
+        <div class="form-entry">
+          <label>Vibe</label>
+          <input type="text" />
+        </div>
+        <button>Submit</button>
       </form>
     </div>
     </>
