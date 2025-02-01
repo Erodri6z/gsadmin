9371f2badcf4606672bcd601c899ea75ca212c30
Erodri6z <erikrodriguez2417@gmail.com>
2025-01-31 20:28:27 -0600

    begin seperating the json and media forms

---
+++ src/components/EditForm/EditPhoto.jsx
@@ -0,0 +1,39 @@
+import { useLocation } from "react-router-dom"
+import { useState } from "react"
+
+const EditPhoto = (props) => {
+  const drinkData = props.drink
+  // const [drinkData, setDrinkData] = useState(drink)
+  const [photoData, setPhotoData] = useState({})
+  
+  const handleSubmitImage = () => {
+    try {
+      props.handleUpdateImage(photoData, drinkData)
+    } catch (err) {
+      console.log("Error submitting image:", err)
+    }
+  }
+  
+  const handleChangePhoto = (e) => {
+    setPhotoData(e.target.files[0])
+  }
+
+  return (
+    <>
+    <h2>Change image</h2>
+    <form onSubmit={handleSubmitImage}>
+        <div class="form-entry">
+          <label>Image</label>
+          <input 
+          type="file" 
+          name="image" 
+          // value={drinkData.image}
+          onChange={handleChangePhoto} />
+        </div>
+        <button>Submit</button>
+      </form>
+    </>
+  )
+}
+
+export default EditPhoto
--- src/pages/DrinkEdit/DrinkEdit.jsx
+++ src/pages/DrinkEdit/DrinkEdit.jsx
@@ -1,19 +1,31 @@
 import { useLocation } from "react-router-dom"
+import { useState } from "react"
 import DrinkEdit from "../../components/EditForm/EditForm"
+import EditPhoto from "../../components/EditForm/EditPhoto"
 
 const DrinkEditPage = (props) => {
   const location = useLocation()
   // console.log(location.state.thisDrink)
-
+  const [drinkSwitch, setDrinkSwitch] = useState(true)
   const drink = location.state.thisDrink
   const { image, ...drinkNoImg } = drink
+
+  const switchForm = () => {
+    drinkSwitch === true?setDrinkSwitch(false):setDrinkSwitch(true)
+  }
+  
   console.log(drink)
   console.log(drinkNoImg)
 
   return (
     <>
+    <button onClick={() => switchForm() }>Switch Between Image/JSON</button>
     <h2>Drink Edit</h2>
-    <DrinkEdit drink={drinkNoImg} handleUpdateImage={props.handleUpdateImage} handleUpdateDrink={props.handleUpdateDrink}/>
+    {drinkSwitch===true?
+    <DrinkEdit drink={drinkNoImg} handleUpdateDrink={props.handleUpdateDrink}/>
+    :
+    <EditPhoto drink={drink} handleUpdateImage={props.handleUpdateImage}/>
+    }
     </>
   )
 }
